393fdea399fa919a2df731c99a81fd47036e3d76
Mikolaj <mwitwicki@edu.cdv.pl>
2024-03-02 21:08:41 +0100

    zmiana poruszania sie

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,19 +2,28 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 10f;
+    public float speed = 10.0f;
+    [SerializeField] private Rigidbody2D rb;
+    private float horizontalInput;
+    private float verticalInput;
+
 
     private void Update()
     {
-        float horizontalInput = Input.GetAxisRaw("Horizontal");
-        float verticalInput = Input.GetAxisRaw("Vertical"); // Pobiera input x i y
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+        
+    }
 
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    void MovePlayer()
+    {
         Vector2 movement = new Vector2(horizontalInput, verticalInput);
-
-        if (movement.magnitude > 0.1f) // Magnitude sprawdza czy gracz siê porusza w jakimœ kierunku
-        {
-            movement.Normalize(); // Dziêki temu nawet id¹c po przek¹tnej mamy tak¹ sam¹ szybkoœæ ruchu jak id¹c w bok
-            transform.position += (Vector3)movement * speed * Time.deltaTime; // Nadanie nowej pozycji
-        }
+        movement.Normalize(); // Normalizacja wektora ruchu
+        rb.velocity = movement * speed;
     }
 }
